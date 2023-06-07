85cb7be9475535aed1c2d4097d52f51646803f7f
kingdeej <devonhenry10196@gmail.com>
2023-06-07 18:36:53 -0500

    main

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './pages/HomePage';
+// import {  } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-        hello world
+        <HomePage />
     </div>
   );
 }
---
+++ src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Nav from '../components/Nav'
+import Hero from '../components/Hero'
+import Footer from '../components/Footer'
+
+export default function HomePage() {
+  return (
+    <main>
+        <Nav />
+        <Hero />
+        <Footer />
+    </main>
+  )
+}
